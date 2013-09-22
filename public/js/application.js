e9ddd54351c08d287a13339f3513ea790c97f7fe
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-22 13:27:30 -0500

    form on index page

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,62 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+google.maps.event.addDomListener(window, 'load', initialize);
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+var geocoder;
+var map;
+
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(41.9483, -87.6556);
+  var mapOptions = {
+    zoom: 14,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  infowindow = new google.maps.InfoWindow({ maxWidth: 320 }); 
+}
+
+function codeAddress() {
+  var address = document.getElementById('zip').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+function setMarker(lat, lon, html) {
+  var latLng = new google.maps.LatLng(lat, lon);
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+    data: html
+  });
+  
+  google.maps.event.addListener(marker, 'click', function() { 
+    map.setCenter(new google.maps.LatLng(marker.position.lat(), marker.position.lng())); 
+    map.setZoom(16); 
+    onItemClick(event, marker); 
+  }); 
+}
+
+function onItemClick(event, pin) { 
+  // Create content
+  var contentString = pin.data
+  // Replace our Info Window's content and position 
+  infowindow.setContent(contentString); 
+  infowindow.setPosition(pin.position); 
+  infowindow.open(map) 
+} 
+
+$(document).ready(function() {
+
+  $('#srch_map').on('submit', function(event) {
+    event.preventDefault();
+    codeAddress();
+  });
+
 });
+
